876aab0dfb79aedabcac12f46c0224b9a858006d
Pedro Reis <pedluhi@outlook.com>
2023-07-05 11:08:43 -0300

    chore: change localStorage theme checker.

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -1,3 +1,4 @@
+import { isCustomErrorPage } from 'next/dist/build/utils';
 import { useState, useEffect } from 'react';
 
 function usePersistedState(key: string, initialState: any) {
@@ -5,10 +6,15 @@ function usePersistedState(key: string, initialState: any) {
   const isClient = typeof window !== 'undefined';
 
   const [state, setState] = useState(() => {
-    
     const storageValue = isClient && localStorage.getItem(key);
 
     if (storageValue) {
+      const storageObj = JSON.parse(storageValue);
+      const storageKeys = Object.keys(storageObj.colors).sort();
+      const initialKeys = Object.keys(initialState.colors).sort();
+      if(JSON.stringify(storageKeys) !== JSON.stringify(initialKeys)){
+        return localStorage.removeItem(key);
+      };
       return JSON.parse(storageValue);
     };
     return initialState;
