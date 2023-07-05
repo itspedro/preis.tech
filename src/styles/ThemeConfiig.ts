174f4aada1df9f1a25141c6806657eba64ee0637
Pedro Reis <pedluhi@outlook.com>
2023-07-05 11:08:15 -0300

    chore: change global style.

--- src/styles/ThemeConfiig.ts
+++ src/styles/ThemeConfiig.ts
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
+import svgToMiniDataURI from 'mini-svg-data-uri';
 
 export const GlobalStyles = createGlobalStyle`
     body {
-        background: ${(props) => props.theme.background};
-        color: ${(props) => props.theme.text};
+        background-color: ${props => props.theme.background};
+        color: ${props => props.theme.textBold};
+        background-size: 40px 40px;
+        background-image: radial-gradient(circle, ${props => props.theme.text20} 1px, rgba(0, 0, 0, 0) 1px);
     };
+
 `
