bea212b5caf7cb470cf742f312afa6739592bb57
Pedro <pedluhi@outlook.com>
2023-07-07 18:00:14 -0300

    [+]style: modify some posts styles.

--- src/components/MDXstyle.tsx
+++ src/components/MDXstyle.tsx
@@ -1,21 +1,73 @@
+'use client'
+
 import styled from 'styled-components';
 
 const Container = styled.article`
+  display: flex;
+  flex-direction: column;
   width: 70%;
   margin: 0 auto;
-  padding: 16px;
   font-family: inherit;
-
+  padding: 200px 16px;
+  font-weight: 400;
+  line-height: 1.8;
 
   h1 {
     font-family: inherit;
     font-size: 32px;
     font-weight: 600;
-    margin-bottom: 16px;
+    margin-bottom: 50px;
   };
 
-  h2 {
+  blockquote {
+    font-size: 18px;
+    font-style: italic;
+    margin: 20px 0;
+    padding: 10px;
+    border-left: 5px solid ${props => props.theme.primary};
+    background: ${props => props.theme.secondary};
+    border-radius: 4px;
+  };
 
+
+  a {
+    color: ${props => props.theme.primary};
+    text-decoration: none;
+    font-weight: 600;
+
+    &:hover {
+      text-decoration: underline;
+    };
+  };
+
+
+
+  p {
+    margin: 20px 0;
+  };
+
+  li {
+    margin: 10px 0;
+  };
+
+  ol {
+    margin: 20px 0;
+    padding-left: 20px;
+  };
+
+  pre {
+    font-size: 14px;
+    background: ${props => props.theme.secondary};
+    padding: 10px;
+    border-radius: 4px;
+    max-width: 700px;
+  };
+
+  code {
+    font-size: 14px;
+    background: ${props => props.theme.secondary};
+    padding: 5px;
+    border-radius: 4px;
   };
 
 `
