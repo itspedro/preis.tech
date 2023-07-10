f30d08ad3da1f7ead9969e9f66fe470f26af92e5
Pedro <pedluhi@outlook.com>
2023-07-10 18:10:00 -0300

    [+]chore: change next config.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   compiler: {
     styledComponents: true
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**'
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
