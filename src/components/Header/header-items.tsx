f35c1c47571d3ed9b547132f1ff83cfe40e767e9
Pedro Reis <pedluhi@outlook.com>
2023-07-13 11:29:57 -0300

    [+]fix: link projects href

--- src/components/Header/header-items.tsx
+++ src/components/Header/header-items.tsx
@@ -41,9 +41,9 @@ function HeaderItems() {
       <Link href="/blog">
         <NavItem>Blog</NavItem>
       </Link>
-      <a href="https:/github.com/itsPedro?tab=repositories">
+      <Link href="https:/github.com/itsPedro?tab=repositories" passHref={true}>
         <NavItem>Projetos</NavItem>
-      </a>
+      </Link>
     </NavList>
   );
 };
