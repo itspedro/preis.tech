4f2190b041e99de19453619754499909763bccf7
Pedro Reis <pedluhi@outlook.com>
2023-08-14 17:15:47 -0300

    [+]chore: add toolbox icons label on hover.

--- src/contents/index/Main/main-toolbox.tsx
+++ src/contents/index/Main/main-toolbox.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { styled } from 'styled-components';
-//import only once the icons that you will use
 
 import { TypeScriptIcon } from '@/components/icons/typescript-icon';
 import { VScodeIcon } from '@/components/icons/vscode-icon';
 import { TailWindIcon } from '@/components/icons/tailwind-icon';
 import { ReactIcon } from '@/components/icons/react-icon';
 import { FigmaIcon } from '@/components/icons/figma-icon';
-import { JavaScriptIcon } from '@/components/icons/javascript-icon';
+import { DartIcon } from '@/components/icons/dart-icon';
 import { NodejsIcon  } from '@/components/icons/nodejs-icon';
+import { FlutterIcon } from '@/components/icons/flutter-icon';
+import { LineIcon } from '@/components/icons/line-icon';
 
 
 const ToolBoxContent = styled.div`
@@ -26,16 +27,18 @@ const ToolBoxText = styled.p`
 const ToolBoxIcons = styled.div`
   display: flex;
   align-items: center;
-  gap: 10px;
 `
 
 const ToolBoxIcon = styled.div<{color: string}>`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   color: ${props => props.theme.text70};
   transition: 0.3s;
   scale: 1;
+  width: 30px;
+  height: 30px;
 
   &:hover {
     color: ${props => props.color};
@@ -43,6 +46,22 @@ const ToolBoxIcon = styled.div<{color: string}>`
   };
 `
 
+const IconLabel = styled.span`
+  position: absolute;
+  font-size: 12px;
+  font-family: inherit;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  display: none;
+  color: ${props => props.theme.text70};
+  top: 30px;
+
+  ${ToolBoxIcon}:hover & {
+    display : block;
+  };
+
+`
 
 function ToolBox() {
   return(
@@ -51,26 +70,38 @@ function ToolBox() {
         Stack/Ferramentas atuais:
       </ToolBoxText>
       <ToolBoxIcons>
-        <ToolBoxIcon color="#FFCA28">
-          <JavaScriptIcon />
-        </ToolBoxIcon>
         <ToolBoxIcon color="#007ACC">
           <TypeScriptIcon />
+          <IconLabel>Typescript</IconLabel>
+        </ToolBoxIcon>
+        <ToolBoxIcon color="#00A8E1">
+          <DartIcon />
+          <IconLabel>Dart</IconLabel>
         </ToolBoxIcon>
         <ToolBoxIcon color="#83CD29">
           <NodejsIcon />
-        </ToolBoxIcon>
-        <ToolBoxIcon color="#61DAFB">
-          <ReactIcon />
+          <IconLabel>NodeJS</IconLabel>
         </ToolBoxIcon>
         <ToolBoxIcon color="#38B2AC">
           <TailWindIcon />
+          <IconLabel>TailwindCSS</IconLabel>
         </ToolBoxIcon>
+        <ToolBoxIcon color="#61DAFB">
+          <ReactIcon />
+          <IconLabel>React</IconLabel>
+        </ToolBoxIcon>
+        <ToolBoxIcon color="#39CEFD">
+          <FlutterIcon />
+          <IconLabel>Flutter</IconLabel>
+        </ToolBoxIcon>
+        <LineIcon />
         <ToolBoxIcon color="#F24E1E">
           <FigmaIcon />
+          <IconLabel>Figma</IconLabel>
         </ToolBoxIcon>
         <ToolBoxIcon color="#3C99D4">
           <VScodeIcon />
+          <IconLabel>VSCode</IconLabel>
         </ToolBoxIcon>
       </ToolBoxIcons>
     </ToolBoxContent>
