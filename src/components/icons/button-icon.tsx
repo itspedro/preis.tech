c5203c5a34930316a1dfd5daa9cf5b5932ed31de
Pedro Reis <pedluhi@outlook.com>
2023-08-14 17:12:40 -0300

    [+]chore: change icons size.

--- src/components/icons/button-icon.tsx
+++ src/components/icons/button-icon.tsx
@@ -1,7 +1,7 @@
 export function ButtonIcon() {
   return (
-    <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9 4.26795C10.3333 5.03775 10.3333 6.96225 9 7.73205L3 11.1962C1.66667 11.966 1.55068e-07 11.0037 1.36708e-07 9.4641L5.409e-08 2.5359C3.57305e-08 0.996296 1.66667 0.0340475 3 0.803848L9 4.26795Z" fill="#55BDB3"/>
+    <svg width="11" height="14" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6.1875 4.9H9.96875L6.1875 1.05V4.9ZM1.375 0H6.875L11 4.2V12.6C11 13.3732 10.3844 14 9.625 14H1.375C0.611875 14 0 13.37 0 12.6V1.4C0 0.623 0.611875 0 1.375 0ZM7.5625 11.2V9.8H1.375V11.2H7.5625ZM9.625 8.4V7H1.375V8.4H9.625Z" fill="currentColor"/>
     </svg>
   );
 };
---
+++ src/components/icons/flutter-icon.tsx
@@ -0,0 +1,48 @@
+export function FlutterIcon() {
+  return (
+    <svg width="17" height="21" viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<mask id="mask0_102_10" maskUnits="userSpaceOnUse" x="0" y="0" width="17" height="21">
+<path d="M16.2984 10.0209L10.9148 15.4064L16.2984 20.792H10.1465L4.76098 15.4064L10.1465 10.0209H16.2984ZM10.1465 0.79213L0.147556 10.7911L3.22446 13.868L16.2984 0.79213H10.1465V0.79213Z" fill="white"/>
+</mask>
+<g mask="url(#mask0_102_10)">
+<path d="M4.76291 15.4064L7.83788 18.4833L16.2984 10.0209H10.1465L4.76291 15.4064Z" fill="currentColor"/>
+</g>
+<mask id="mask1_102_10" maskUnits="userSpaceOnUse" x="0" y="0" width="17" height="21">
+<path d="M16.2984 10.0209L10.9148 15.4064L16.2984 20.792H10.1465L4.76098 15.4064L10.1465 10.0209H16.2984ZM10.1465 0.79213L0.147556 10.7911L3.22446 13.868L16.2984 0.79213H10.1465V0.79213Z" fill="white"/>
+</mask>
+<g mask="url(#mask1_102_10)">
+<path d="M10.1465 0.79213H16.2984L3.22446 13.868L0.147556 10.7911L10.1465 0.79213Z" fill="currentColor"/>
+</g>
+<mask id="mask2_102_10" maskUnits="userSpaceOnUse" x="0" y="0" width="17" height="21">
+<path d="M16.2984 10.0209L10.9148 15.4064L16.2984 20.792H10.1465L4.76098 15.4064L10.1465 10.0209H16.2984ZM10.1465 0.79213L0.147556 10.7911L3.22446 13.868L16.2984 0.79213H10.1465V0.79213Z" fill="white"/>
+</mask>
+<g mask="url(#mask2_102_10)">
+<path d="M7.83788 18.4833L10.1465 20.79H16.2984L10.9148 15.4064L7.83788 18.4833Z" fill="currentColor"/>
+</g>
+<mask id="mask3_102_10" maskUnits="userSpaceOnUse" x="0" y="0" width="17" height="21">
+<path d="M16.2984 10.0209L10.9148 15.4064L16.2984 20.792H10.1465L4.76098 15.4064L10.1465 10.0209H16.2984ZM10.1465 0.79213L0.147556 10.7911L3.22446 13.868L16.2984 0.79213H10.1465V0.79213Z" fill="white"/>
+</mask>
+<g mask="url(#mask3_102_10)">
+<path d="M12.4012 16.9025L10.9148 15.4064L7.83788 18.4833L12.4012 16.9025Z" fill="url(#paint0_linear_102_10)"/>
+</g>
+<mask id="mask4_102_10" maskUnits="userSpaceOnUse" x="0" y="0" width="17" height="21">
+<path d="M16.2984 10.0209L10.9148 15.4064L16.2984 20.792H10.1465L4.76098 15.4064L10.1465 10.0209H16.2984ZM10.1465 0.79213L0.147556 10.7911L3.22446 13.868L16.2984 0.79213H10.1465V0.79213Z" fill="white"/>
+</mask>
+<g mask="url(#mask4_102_10)">
+<path d="M4.76256 15.4054L7.83953 12.3284L10.9167 15.4054L7.83953 18.4824L4.76256 15.4054Z" fill="currentColor"/>
+</g>
+<path d="M16.2984 10.0209L10.9148 15.4064L16.2984 20.792H10.1465L4.76098 15.4064L10.1465 10.0209H16.2984ZM10.1465 0.79213L0.147556 10.7911L3.22446 13.868L16.2984 0.79213H10.1465Z" fill="url(#paint1_radial_102_10)"/>
+<defs>
+<linearGradient id="paint0_linear_102_10" x1="8.73582" y1="19.0771" x2="11.0274" y2="16.7855" gradientUnits="userSpaceOnUse">
+<stop stop-color="currentColor" stop-opacity="0.4"/>
+<stop offset="1" stop-color="currentColor" stop-opacity="0"/>
+</linearGradient>
+<radialGradient id="paint1_radial_102_10" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(0.601288 2.15526) scale(24.4674)">
+<stop stop-color="white" stop-opacity="0.1"/>
+<stop offset="1" stop-color="white" stop-opacity="0"/>
+</radialGradient>
+</defs>
+</svg>
+
+  );
+};
