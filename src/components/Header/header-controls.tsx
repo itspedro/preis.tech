f2f2f15d4b620aca36ebedfbb95ffb83a1d30496
Pedro Reis <pedluhi@outlook.com>
2023-08-14 17:13:14 -0300

    [+]chore: change line icon.

--- src/components/Header/header-controls.tsx
+++ src/components/Header/header-controls.tsx
@@ -4,14 +4,7 @@ import { LightThemeIcon } from '@/components/icons/light-theme-icon';
 import Link from 'next/link';
 import useTheme from '@/hooks/useTheme';
 import { DarkThemeIcon } from '@/components/icons/dark-theme-icon';
-
-function Line() {
-  return (
-    <svg width="1" height="12" viewBox="0 0 1 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <line x1="0.5" y1="2.18557e-08" x2="0.499999" y2="12" stroke="#C0C0C0"/>
-    </svg>
-  );
-};
+import { LineIcon } from '@/components/icons/line-icon';
 
 const ControlContainer = styled.div`
   display: flex;
@@ -63,7 +56,7 @@ function HeaderControl() {
           <GithubIcon />
         </Icons>
       </Link>
-      <Line />
+      <LineIcon />
       <ToggleThemeButton onClick={toggleTheme}>
         {themeTitle === 'light' ? <DarkThemeIcon /> : <LightThemeIcon />}
       </ToggleThemeButton>
---
+++ src/components/icons/line-icon.tsx
@@ -0,0 +1,7 @@
+export function LineIcon() {
+  return (
+    <svg width="1" height="12" viewBox="0 0 1 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="0.5" y1="2.18557e-08" x2="0.499999" y2="12" stroke="#C0C0C0"/>
+    </svg>
+  );
+};
