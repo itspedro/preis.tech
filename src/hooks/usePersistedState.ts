a7a6108da520f37241a67c25259d8f2596fdc502
Pedro Reis <itspdro@outlook.com>
2024-01-15 21:47:13 -0300

    fix: hydration error

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -1,30 +1,35 @@
+'use client'
+
 import { useState, useEffect } from 'react';
 
 function usePersistedState(key: string, initialState: any) {
-
+  if (typeof window === 'undefined') {
+    throw new Error('usePersistedState must be used on the client side.');
+  }
   const isClient = typeof window !== 'undefined';
 
   const [state, setState] = useState(() => {
-    const storageValue = isClient && localStorage.getItem(key);
-
-    if (storageValue) {
-      const storageObj = JSON.parse(storageValue);
-      const storageKeys = Object.keys(storageObj.colors).sort();
-      const initialKeys = Object.keys(initialState.colors).sort();
-      if(JSON.stringify(storageKeys) !== JSON.stringify(initialKeys)){
-        return localStorage.removeItem(key);
-      };
-      return JSON.parse(storageValue);
-    };
+    if (isClient) {
+      const storageValue = localStorage.getItem(key);
+      if (storageValue) {
+        const storageObj = JSON.parse(storageValue);
+        const storageKeys = Object.keys(storageObj.colors).sort();
+        const initialKeys = Object.keys(initialState.colors).sort();
+        if (JSON.stringify(storageKeys) !== JSON.stringify(initialKeys)) {
+          localStorage.removeItem(key);
+          return initialState;
+        }
+        return JSON.parse(storageValue);
+      }
+    }
     return initialState;
   });
-  
+
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state))
+    localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
 
   return [state, setState];
-
-};
+}
 
 export default usePersistedState;
