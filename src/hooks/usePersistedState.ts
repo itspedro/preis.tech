c5b57b9fa0cee1cc9801cd8daa3e1a5e89c3d21a
Pedro Reis <itspdro@outlook.com>
2024-01-23 08:31:25 -0300

    [+]chore: update persisted state

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -1,19 +1,23 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { getCookie, setCookie } from 'cookies-next';
 
 function usePersistedState<T>(key: string, initialState: T) {
   const [state, setState] = useState<T>(initialState);
 
   useEffect(() => {
-    if (typeof window === 'undefined') return;
-    let storageValue = localStorage.getItem(key);
-    if (storageValue) setState(JSON.parse(storageValue));
+    if (typeof window === 'undefined') return
+    const storageValue = getCookie(key);
+
+    if (storageValue) {
+      setState(JSON.parse(storageValue));
+    }
   }, [key]);
 
   const updateLocalStorage = (state: T) => {
+    setCookie(key, JSON.stringify(state));
     setState(state);
-    localStorage.setItem(key,JSON.stringify(state));
   }
 
   return {
