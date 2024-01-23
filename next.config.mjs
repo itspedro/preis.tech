33cd9cbeb9fb25679e4a38a691c64eade77a80de
Pedro Reis <itspdro@outlook.com>
2024-01-23 13:13:10 -0300

    [+]chore: add dribbble cdn link

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,12 @@ const nextConfig = {
         port: '',
         pathname: '/**'
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+        port: '',
+        pathname: '/**'
+      }
     ],
   },
 }
